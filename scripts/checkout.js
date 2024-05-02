6a8a964ddf5363a2ee32db9bab2d466fc955442a
Nur <cocshishir@gamil.com>
2024-05-02 14:33:18 +0600

    Delete product

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
    });
 
    cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${productId}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -99,9 +99,12 @@ cart.forEach((cartItem) => {
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
-document.querySelectorAll(`.js-delete-link`).forEach((link) => {
-    link.addEventListener(`click`, () => {
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+    link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFormCart(productId);
+
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     });
 });
